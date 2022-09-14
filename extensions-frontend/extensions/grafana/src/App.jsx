d616d63464534ede00d359c635f5feee4789a0fe
donniean <acmilank22@gmail.com>
2022-09-14 11:40:15 +0800

    feat: init grafana

--- extensions-frontend/extensions/grafana/src/App.jsx
+++ extensions-frontend/extensions/grafana/src/App.jsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useRef } from 'react';
+import { Loading } from '@kubed/components';
 
-const Wrapper = styled.h3`
-  margin: 8rem auto;
-  text-align: center;
-`;
+const FRAME_URL = '/proxy/grafana/';
 
 export default function App() {
-  return <Wrapper>Grafana</Wrapper>;
+  const [loading, setLoading] = useState(true);
+  const iframeRef = useRef();
+
+  const onIframeLoad = () => {
+    const iframeDom = iframeRef.current?.contentWindow.document;
+    if (iframeDom) {
+      // if (iframeDom.querySelector('#app > div > div.header > div')) {
+      //   iframeDom.querySelector('#app > div > div.header > div').style.display = 'none';
+      // }
+    }
+    setLoading(false);
+  };
+
+  return (
+    <>
+      {loading && <Loading className="page-loading" />}
+      <iframe
+        ref={iframeRef}
+        src={FRAME_URL}
+        width="100%"
+        height="100%"
+        style={{
+          height: 'calc(100vh - 68px)',
+          display: loading ? 'none' : 'block',
+        }}
+        onLoad={onIframeLoad}
+      />
+    </>
+  );
 }
